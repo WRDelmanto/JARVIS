22155bd80ca9da36050f904860d88e6e4f7e2996
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2020-06-26 18:12:11 -0300

    Prettier - Code formatter

--- index.js
+++ index.js
@@ -1,131 +1,149 @@
 //Just open the Terminal and write "node ." to start J.A.R.V.I.S.
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
-const ms = require('ms');
-const ping = require('minecraft-server-util');
+const ms = require("ms");
+const ping = require("minecraft-server-util");
 
-const prefix = 'j!';
+const prefix = "j!";
 
-var version =  '1.5.0'
-//              X.Y.Z 
+var version = "1.5.0";
+//              X.Y.Z
 //              X = First bot
 //                Y = How many functions the bot have
 //                  Z = How many modifications after the last function was added
 
-const Bot_Creator = 'Besides Tony, WRDelmanto'
-const Bot_Color = '0x008080'
+const Bot_Creator = "Besides Tony, WRDelmanto";
+const Bot_Color = "0x008080";
 
-const Bot_Image_1280x720 = new Discord.MessageAttachment('./images/Discord Bot 1280x720.png')
-const Bot_Image_1024x1024 = new Discord.MessageAttachment('./images/Discord Bot 1024x1024.png')
-const Bot_Image_Right = new Discord.MessageAttachment('./images/Discord Bot Right.png')
-const Bot_Image_Left = new Discord.MessageAttachment('./images/Discord Bot Left.png')
+const Bot_Image_1280x720 = new Discord.MessageAttachment(
+  "./images/Discord Bot 1280x720.png"
+);
+const Bot_Image_1024x1024 = new Discord.MessageAttachment(
+  "./images/Discord Bot 1024x1024.png"
+);
+const Bot_Image_Right = new Discord.MessageAttachment(
+  "./images/Discord Bot Right.png"
+);
+const Bot_Image_Left = new Discord.MessageAttachment(
+  "./images/Discord Bot Left.png"
+);
 
-bot.on('ready', () =>{
-    console.clear();
-    console.log('- J.A.R.V.I.S. is Online');
-    bot.user.setActivity('j!help', {type: 'LISTENING'}).catch(console.error);
-})
+bot.on("ready", () => {
+  console.clear();
+  console.log("- J.A.R.V.I.S. is Online");
+  bot.user.setActivity("j!help", { type: "LISTENING" }).catch(console.error);
+});
 
-bot.on('message', message=>{
+bot.on("message", (message) => {
+  let args = message.content.substring(prefix.length).split(" ");
 
-    let args = message.content.substring(prefix.length).split(" ");
+  switch (args[0]) {
+    case "help":
+      const helper = new Discord.MessageEmbed()
+        .setTitle("**What can I do for you today sir?**")
+        .addField(prefix + "spoll", "Create a simple YES or NO poll")
+        .addField(
+          prefix + "clear",
+          "Delete some lines from your current text channel"
+        )
+        .addField(prefix + "ms", "Show information of a Minecraft Server")
+        .addField(prefix + "info", "Reveal J.A.R.V.I.S. information")
+        //.addField(prefix + 'poll', 'Create a augmented poll')
+        //.addField('Current Server', message.guild.name, true)
+        .setColor(Bot_Color)
+        .setFooter("J.A.R.V.I.S. " + version);
+      //.setFooter('Creator: ' + Bot_Creator)
+      message.channel.send(helper);
+      break;
 
-    switch(args[0]){
-        case 'help':
-            const helper = new Discord.MessageEmbed()
-            .setTitle('**What can I do for you today sir?**')
-            .addField(prefix + 'spoll', 'Create a simple YES or NO poll')
-            .addField(prefix + 'clear', 'Delete some lines from your current text channel')
-            .addField(prefix + 'ms', 'Show information of a Minecraft Server')
-            .addField(prefix + 'info', 'Reveal J.A.R.V.I.S. information')
-            //.addField(prefix + 'poll', 'Create a augmented poll')
-            //.addField('Current Server', message.guild.name, true)
-            .setColor(Bot_Color)
-            .setFooter('J.A.R.V.I.S. ' + version)
-            //.setFooter('Creator: ' + Bot_Creator)
-            message.channel.send(helper);
+    case "spoll":
+      const simple_poll = new Discord.MessageEmbed()
+        .setTitle("Initiate Simple Poll")
+        .setColor(Bot_Color)
+        .setFooter(
+          "Please type <" +
+            prefix +
+            "spoll Does Palladium increases Tony`s blood toxicity?> for example"
+        );
+
+      if (!args[1]) {
+        message.channel.send(simple_poll);
         break;
+      }
 
-        case 'spoll':
-            const simple_poll = new Discord.MessageEmbed()
-            .setTitle('Initiate Simple Poll')
-            .setColor(Bot_Color)
-            .setFooter('Please type <' + prefix + 'spoll Does Palladium increases Tony`s blood toxicity?> for example')
+      let msgArgs = args.slice(1).join(" ");
 
-            if (!args[1]){
-                message.channel.send(simple_poll)
-                break;
-            }
+      message.channel
+        .send("📋 " + "**" + msgArgs + "**" + " 📋")
+        .then((messageReaction) => {
+          messageReaction.react("👍");
+          messageReaction.react("👎");
+          message.delete({ timeout: 1000 }).catch(console.error);
+        });
+      break;
 
-            let msgArgs = args.slice(1).join(' ');
+    case "info":
+      if (!args[1]) {
+        const info = new Discord.MessageEmbed()
+          .setTitle("Info Menu")
+          .addField(prefix + "info up", "Check if J.A.R.V.I.S. is up")
+          .addField(
+            prefix + "info creator",
+            "We all know Tony needed a little help"
+          )
+          .setFooter("J.A.R.V.I.S. " + version);
 
-            message.channel.send("📋 " + "**" + msgArgs + "**" + " 📋").then(messageReaction => {
-                messageReaction.react("👍");
-                messageReaction.react("👎");
-                message.delete({ timeout: 1000 }).catch(console.error);
-     
-            });
-        break;
+        message.channel.send(info);
+      } else if (args[1] === "up") {
+        message.channel.send(
+          message.author.username + " - J.A.R.V.I.S., are you up?"
+        );
+        message.channel.send("- For you sir, always.");
+      } else if (args[1] === "creator") {
+        const Info_creator = new Discord.MessageEmbed()
+          .setTitle("Creator Information")
+          .addField("Creator: ", Bot_Creator)
+          //.setThumbnail(message.author.displayAvatarURL())
+          .setColor(Bot_Color);
+        message.channel.send(Info_creator);
+      } else message.channel.send("Invalid Arguments");
+      break;
 
-        case 'info':
-            if(!args[1]){
-                const info = new Discord.MessageEmbed()
-                .setTitle('Info Menu')
-                .addField(prefix + 'info up', 'Check if J.A.R.V.I.S. is up')
-                .addField(prefix + 'info creator', 'We all know Tony needed a little help')
-                .setFooter('J.A.R.V.I.S. ' + version)
+    case "clear":
+      if (!args[1]) {
+        message.channel.send(
+          "Please type <" + prefix + "clear 10> for example"
+        );
+      } else if (args[1] > 100)
+        message.channel.send("Cannot delete more than 100 lines");
+      else message.channel.bulkDelete(args[1]);
+      break;
 
-                message.channel.send(info)
-            }
+    case "ms":
+      if (!args[1]) {
+        message.channel.send("You must type a minecraft server ip");
+      }
+      if (!args[2]) {
+        args[2] = "25565"; //return message.channel.send('You must type a minecraft server port')
+      }
 
-            else if(args[1] === 'up'){
-                    message.channel.send(message.author.username + ' - J.A.R.V.I.S., are you up?');
-                    message.channel.send('- For you sir, always.');}
-                else if (args[1] === 'creator'){
-                        const Info_creator = new Discord.MessageEmbed()
-                        .setTitle('Creator Information')
-                        .addField('Creator: ', (Bot_Creator))
-                        //.setThumbnail(message.author.displayAvatarURL())
-                        .setColor(Bot_Color)
-                        message.channel.send(Info_creator);}
-                    else
-                        message.channel.send('Invalid Arguments');
-        break;
+      ping(args[1], parseInt(args[2]), (error, reponse) => {
+        if (error) throw error;
+        const minecraft_server = new Discord.MessageEmbed()
+          .setTitle("Server Status")
+          .addField("Server IP", reponse.host)
+          .addField(
+            "Online/Max Players",
+            reponse.onlinePlayers + "/" + reponse.maxPlayers
+          )
+          .addField("Server Version", reponse.version)
+          .setColor(Bot_Color);
 
-        case 'clear':
-            if(!args[1]){
-                message.channel.send('Please type <' + prefix + 'clear 10> for example')}
-                else if (args[1] > 100)
-                        message.channel.send('Cannot delete more than 100 lines')
-                    else
-                        message.channel.bulkDelete(args[1]);
-        break;
-
-        case 'ms':
- 
-            if(!args[1]) return message.channel.send('You must type a minecraft server ip')
-            if(!args[2]){
-                args[2] = '25565' //return message.channel.send('You must type a minecraft server port')
-            }
- 
-            ping(args[1], parseInt(args[2]), (error, reponse) =>{
-                if(error) throw error
-                const minecraft_server = new Discord.MessageEmbed()
-                .setTitle('Server Status')
-                .addField('Server IP', reponse.host)
-                .addField('Online/Max Players', reponse.onlinePlayers + '/' + reponse.maxPlayers)
-                .addField('Server Version', reponse.version)
-                .setColor(Bot_Color)
-               
-                message.channel.send(minecraft_server)
-            })
-        break
-
-    }
-})
+        message.channel.send(minecraft_server);
+      });
+      break;
+  }
+});
 
 bot.login(process.env.token);
-
-// Goodbye = 'As always sir, a great pleasure watching you work.'
-// message.author.username
