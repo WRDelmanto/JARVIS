7dac2d19ea05c28560db329a195c5bafe1a686d7
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2020-07-07 17:39:37 -0300

    fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ms = require('ms');
 const ping = require('minecraft-server-util');
+const dateformat = require('dateformat');
 //---------------------------------------------------------------------------------
 
 //Command Handler
