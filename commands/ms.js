15c0cc0de4d48ec2de9ab7eea5859f6712b532d7
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2021-02-13 20:16:34 -0300

    ms (Fix)

--- commands/ms.js
+++ commands/ms.js
@@ -9,31 +9,31 @@ const Bot_Color = '0x008080';
 
 //HELP
 module.exports = {
-  name: 'ms_embed',
-  description: 'Display Information on a Minecraft Server',
-  execute(message, args) {
-    if (!args[1]) {
-      message.channel.send('You must type a minecraft server ip');
-    }
-    if (!args[2]) {
-      args[2] = '25565'; //return message.channel.send('You must type a minecraft server port')
-    }
+	name: 'ms_embed',
+	description: 'Display Information on a Minecraft Server',
+	execute(message, args) {
+		if (!args[1]) {
+			message.channel.send('You must type a minecraft server ip');
+		}
+		if (!args[2]) {
+			args[2] = '25565'; //return message.channel.send('You must type a minecraft server port')
+		}
 
-    ping(String(args[1]), parseInt(args[2]), (error, reponse) => {
-      if (error) throw error;
-      console.log(Response);
-      //const minecraft_server = new Discord.MessageEmbed()
-      // .setTitle('Server Status')
-      // .addField('Server IP', reponse.host)
-      // .addField(
-      //   'Online/Max Players',
-      //   reponse.onlinePlayers + '/' + reponse.maxPlayers
-      //)
-      // .addField('Server Version', reponse.version)
-      //.setColor(Bot_Color);
+		ping(String(args[1]), parseInt(args[2]), (error, response) => {
+			if (error) throw error;
+			console.log(Response);
+			//const minecraft_server = new Discord.MessageEmbed()
+			// .setTitle('Server Status')
+			// .addField('Server IP', response.host)
+			// .addField(
+			//   'Online/Max Players',
+			//   response.onlinePlayers + '/' + response.maxPlayers
+			//)
+			// .addField('Server Version', response.version)
+			//.setColor(Bot_Color);
 
-      //message.channel.send(minecraft_server);
-      delete { timeout: Delete_Message_Timeout };
-    });
-  },
+			//message.channel.send(minecraft_server);
+			delete { timeout: Delete_Message_Timeout };
+		});
+	},
 };
