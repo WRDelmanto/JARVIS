396c85c2b541310ba15ddc9c6851aaa6226e0008
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2021-03-14 21:19:12 -0300

    ms (Fix)

--- commands/ms.js
+++ commands/ms.js
@@ -7,33 +7,32 @@ const ping = require('minecraft-server-util');
 const Delete_Message_Timeout = '50';
 const Bot_Color = '0x008080';
 
-//HELP
+//MS
 module.exports = {
 	name: 'ms_embed',
 	description: 'Display Information on a Minecraft Server',
 	execute(message, args) {
-		if (!args[1]) {
-			message.channel.send('You must type a minecraft server ip');
-		}
 		if (!args[2]) {
 			args[2] = '25565'; //return message.channel.send('You must type a minecraft server port')
 		}
+		if (!args[1]) {
+			message.channel.send('You must type a minecraft server ip');
+		} else {
+			ping(String(args[1]), parseInt(args[2]), (error, response) => {
+				if (error) throw error;
+				const minecraft_server = new Discord.MessageEmbed()
+					.setTitle('Server Status')
+					.addField('Server IP', response.host)
+					.addField(
+						'Online/Max Players',
+						response.onlinePlayers + '/' + response.maxPlayers
+					)
+					.addField('Server Version', response.version)
+					.setColor(Bot_Color);
 
-		ping(String(args[1]), parseInt(args[2]), (error, response) => {
-			if (error) throw error;
-			console.log(Response);
-			//const minecraft_server = new Discord.MessageEmbed()
-			// .setTitle('Server Status')
-			// .addField('Server IP', response.host)
-			// .addField(
-			//   'Online/Max Players',
-			//   response.onlinePlayers + '/' + response.maxPlayers
-			//)
-			// .addField('Server Version', response.version)
-			//.setColor(Bot_Color);
-
-			//message.channel.send(minecraft_server);
-			delete { timeout: Delete_Message_Timeout };
-		});
+				message.channel.send(minecraft_server);
+				delete { timeout: Delete_Message_Timeout };
+			});
+		}
 	},
 };
