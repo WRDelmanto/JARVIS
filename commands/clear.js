c6c354d477a3632ab946fb1e00cc558ebf68ce92
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2021-12-16 19:45:32 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,11 +3,13 @@ const Discord = require('discord.js');
 
 //Bot Variables
 const prefix = 'j!';
+const Delete_Message_Timeout = '50';
 
 //TEST
 module.exports = {
   name: 'clear',
   description: 'Clear some lines in current channel',
+  //ownerOnly: true, (Not Working)
   execute(message, args) {
     if (!message.member.hasPermission('MANAGE_MESSAGES')) {
       message.channel.send('Sir, You are not authorized to access this area.');
@@ -16,6 +18,10 @@ module.exports = {
       message.channel.send('Please type <' + prefix + 'clear 10> for example');
     } else if (args[1] > 100)
       message.channel.send('Cannot delete more than 100 lines');
-    else message.channel.bulkDelete(args[1]);
+    else message.channel.send('The Clean Slate Protocol Was Executed.');
+    //time.sleep(Delete_Message_Timeout);
+    try {
+      message.channel.bulkDelete(args[1]);
+    } catch (err) {}
   },
 };
